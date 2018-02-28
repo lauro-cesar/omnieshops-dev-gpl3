5d2efe6e90bae8bd18ac6b2689a8f0ce0531b1a0
Lauro C Oliveira <lauro@hostcert.com.br>
2018-02-28 17:53:40 +0000

    Merging subprojects

---
+++ senchaWorkSpace/build/temp/development/demo/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/olarva/Documents/Projetos/opensource/omnieshops-dev-gpl3/senchaWorkSpace/demo/app.js
---
+++ senchaWorkSpace/build/temp/development/omniDashBoard/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = '/Users/olarva/Documents/Projetos/opensource/omnieshops-dev-gpl3/senchaWorkSpace/build/.sass-cache'
